e84f3ff3404a06532266127c3f46d83318e88d61
ShuhongWangWM <swang32@email.wm.edu>
2021-04-10 19:20:07 +0800

    update gui

--- gui.py
+++ gui.py
@@ -1,8 +1,26 @@
-from tkinter import * 
-from tkinter.ttk import *
+import tkinter as tk
+from PIL import ImageTk, Image
 
-root = Tk()
-root.title("EmoExpresser")
-label = Label(root, text ="Hello World !").pack()
-# Code to add widgets will go here...
+# Main window
+root = tk.Tk()
+root.title("EmoExpressor")
+root.geometry("700x500+300+150")
+root.resizable(width=True, height=True)
+
+# Logo
+img = Image.open("logo.png")
+img = img.resize((350, 250), Image.ANTIALIAS)
+img = ImageTk.PhotoImage(img)
+panel = tk.Label(root, image = img)
+panel.pack(side = "top", fill = "both", expand = 0)
+
+# Feedback
+message = tk.Label(root, text="You looks ..., do you want to try these?", width=200, font=("Arial", 25), anchor="w", height = 2)
+message.pack()
+
+# Emoji
+T = tk.Text(root, height=7, width=100)
+T.pack()
+T.insert(tk.END, 'Your emoji goes here...')
+
 root.mainloop()
