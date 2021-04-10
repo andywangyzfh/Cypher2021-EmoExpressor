6c33641f7e8a6b5669abd277710fb304a354da36
Kevincklhhh <Kevin_ckl@outlook.com>
2021-04-10 17:51:51 +0800

    Add files via upload
    able to handle emotes

--- guitest.py
+++ guitest.py
@@ -1,3 +1,40 @@
+<<<<<<< HEAD
+from tkinter import * 
+from tkinter.ttk import *
+import re
+root = Tk()
+root.title("EmoExpresser")
+label = 'Angry'
+w = Label(root, text ='Your emote', font = "50") 
+w.pack()
+_nonbmp = re.compile(r'[\U00010000-\U0010FFFF]')
+def _surrogatepair(match):
+    char = match.group()
+    assert ord(char) > 0xffff
+    encoded = char.encode('utf-16-le')
+    return (
+        chr(int.from_bytes(encoded[:2], 'little')) + 
+        chr(int.from_bytes(encoded[2:], 'little')))
+def with_surrogates(text):
+    return _nonbmp.sub(_surrogatepair, text)
+def display_emote():
+    #label = label update the label based on emote returned from detector
+    if label == 'Angry':
+        w.config(text= with_surrogates('😡'))
+    elif label == 'Happy':
+        w.config(text= with_surrogates('😁'))
+    elif label == 'Neutral':
+        w.config(text= with_surrogates('😐'))
+    elif label == 'Sad':
+        w.config(text= with_surrogates('😢'))
+    elif label == 'Surprise':
+        w.config(text= with_surrogates('😲'))
+    root.after(1000, display_emote)    
+
+root.after(1000, display_emote)
+root.mainloop()
+
+=======
 from tkinter import * 
 from tkinter.ttk import *
 root = Tk()
@@ -22,3 +59,4 @@ def display_emote():
 root.after(1000, display_emote)
 root.mainloop()
 
+>>>>>>> 2045d8f6012058e931f488961e9ca0932cbf6dd2
