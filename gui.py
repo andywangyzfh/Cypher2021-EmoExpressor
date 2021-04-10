90d9f8566036569883042297d68b1d53686bdafd
ShuhongWangWM <swang32@email.wm.edu>
2021-04-10 15:49:08 +0800

    initial commit

---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import * 
+from tkinter.ttk import *
+
+root = Tk()
+root.title("EmoExpresser")
+label = Label(root, text ="Hello World !").pack()
+# Code to add widgets will go here...
+root.mainloop()
